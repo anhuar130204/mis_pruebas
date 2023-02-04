890e070ad8319249f31fc068a503a5d9ddb418c5
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-02-04 22:28:41 +0000

    prueba2 con mensajes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # mis_pruebas
 |No.|Archivo|Descripcion|
 |---|---|----|
-|1|pruba1.py|se realizo piramide de numeros|
+|1|prueba1.py|se realizo piramide de numeros|
+|2|pruaba2.py|añadoiendo texto con introduccion de texto|
---
+++ pruebas/prueba2.py
@@ -0,0 +1,9 @@
+"""
+   Prueba1
+   Nombre:Anhuar Fernando MI                                              
+   Fecha: 02/02/2023                                                      
+   Descripcion:piramide de nuemros
+"""
+mensaje = str(input("escribe mensaje:"))
+mensaje += str("  mensaje1")
+print(mensaje)
