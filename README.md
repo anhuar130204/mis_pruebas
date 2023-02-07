edc3aefb4fe092e8568a10cb5c8d99b1e18c5bfb
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-02-07 04:59:17 +0000

    Lista de números

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |1|prueba1.py|se realizo piramide de numeros|
 |2|pruaba2.py|añadoiendo texto con introduccion de texto|
 |3|prueba3.py|sentencia if|
+|4|prueba4.py|listas|
---
+++ pruebas/prueba4.py
@@ -0,0 +1,9 @@
+"""
+   Prueba4
+   Nombre:Anhuar Fernando MI                                              
+   Fecha: 05/02/2023                           6                          
+   Descripcion: lista de números 
+"""
+numeros  = [7, 7, 7, 7, 7]  # asignación números como lista 
+numeros [2] = 5   # cambiar el elemento 2 de la lista números por 5
+print(numeros)  # imprimir lista numeros
